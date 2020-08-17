dfa3c5d257a754d7c2e16d6951169377a1d294a6
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-08-17 11:29:33 +1000

    upto task 3

--- wk4.sql
+++ wk4.sql
@@ -15,13 +15,77 @@ go
 EXEC MULTIPLY1 @num1 = 2, @num2 = 3;
 */
 
-CREATE FUNCTION ADD4 (@num1 INT, @num2 INT) RETURNS INT
+/* ------------------------------ Task 2
+
+
+I know how to create functions but whenever i entered strings 
+into the RETURN CONCAT, I got this error:
+"Conversion failed when converting the varchar value '2 + 3 = 5' to data type int"
+Just settled for listing the parameters and output
+
+
+
+
+DROP FUNCTION IF EXISTS [ADD];
+GO
+
+CREATE FUNCTION [ADD] (@num1 INT, @num2 INT) RETURNS INT AS
 BEGIN
-    RETURN CONCAT (@num1, ' + ', @num2, ' = ', @num1 + @num2)    
+    RETURN @num1 + @num2;    
 END;
 
 go
 
 BEGIN
-    SELECT dbo.ADD4(2, 3);
+DECLARE @RESULT INT;
+EXEC @RESULT = ADD5 @num1 = 2, @num2 = 3;
+    SELECT CONCAT(@num1, ' + ', @num2, ' = ', @RESULT);
 END;
+*/
+
+/* ------------------------------------ Task 3
+
+
+GO
+
+CREATE TABLE ACCOUNT1 (
+    AcctNo INT,
+    Fname NVARCHAR(100),
+    Lname NVARCHAR(100),
+    CreditLimit MONEY,
+    Balance MONEY,
+    PRIMARY KEY (AcctNo)
+)
+
+CREATE TABLE LOG (
+    OrigAcct INT,
+    LogDateTime DATETIME,
+    RecAcct INT,
+    Amount MONEY,
+    FOREIGN KEY (OrigAcct) references Account,
+    FOREIGN KEY (RecAcct) references Account,
+)
+
+
+GO
+
+INSERT INTO ACCOUNT1 (AcctNo, Fname, Lname, CreditLimit, Balance) VALUES
+(1, 'Dom', 'Hudson', 1000, 500),
+(2, 'Joel', 'Malcolm', 2000, 700)
+*/
+
+DROP procedure if exists TRANSFER;
+
+GO
+
+Create procedure TRANSFER @FromAcct INT, @ToAcct INT, @Amnt MONEY AS
+BEGIN 
+    update ACCOUNT set @FromAcct = AcctNo, @ToAcct = AcctNo, balance = balance - @Amnt where AcctNo = @FromAcct;
+END;
+
+Exec TRANSFER @FromAcct = 1, @ToAcct = 2, @Amnt = 400
+
+go
+
+SELECT *
+FROM ACCOUNT1;
